480916bae2df20a8ec9dbe7e594ac5ebf6bab7d2
Roman92P <roman.paszkow@gmail.com>
2023-05-21 23:19:57 +0200

    first  cpp program

--- HelloWorldCpp.cpp
+++ HelloWorldCpp.cpp
@@ -1,7 +1,21 @@
-﻿#include <iostream>
+﻿#include <iostream>  // for std::cout and std::cin
 
 int main()
 {
-	std::cout << "Hello, world!";
-	return 0;
+    //std::cout << "Enter two numbers separated by a space: ";
+
+    //int x{ }; // define variable x to hold user input (and zero-initialize it)
+    //int y{ }; // define variable y to hold user input (and zero-initialize it)
+    //std::cin >> x >> y; // get two numbers and store in variable x and y respectively
+
+    //std::cout << "You entered " << x << " and " << y << '\n';
+    
+    std::cout << "Enter an integer: ";
+    int x{ };
+    std::cin >> x;
+    x = x * 2;
+    std::cout << "Double that number is: " << x << "\n";
+    
+
+    return 0;
 }
